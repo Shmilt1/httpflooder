2ad2df449a227cf2dc9bc18c624cab010ddd5cac
= <poopfave@protonmail.com>
2025-09-21 13:31:39 +0000

    Updated main.go, etc.

--- main.go
+++ main.go
@@ -1,15 +1,57 @@
 package main
 
-import "httpflooder/internals"
+import (
+	"flag"
+	"httpflooder/internals"
+	"sync"
+)
 
 func main() {
-	flooder := &internals.HttpFlooder{
-		Host:     "127.0.0.1",
-		Port:     80,
-		Secure:   false,
-		Interval: 2,
-		Duration: 60,
+	host := flag.String("h", "127.0.0.1", "Target host")
+	port := flag.Int("p", 80, "Target port")
+	duration := flag.Int("d", 60, "Duration of flood")
+	interval := flag.Int("i", 0, "Interval per requests")
+	threads := flag.Int("t", 2, "Threads")
+	secure := flag.Bool("s", false, "Target is HTTPS")
+
+	flag.Parse()
+
+	if *duration == 0 {
+		*duration = 60
 	}
 
-	flooder.Flood()
+	var wg sync.WaitGroup
+
+	if *host != "" && *port != 0 {
+		if *threads == 0 {
+			// ...
+			flooder := internals.HttpFlooder{
+				Host:     *host,
+				Port:     *port,
+				Duration: *duration,
+				Interval: *interval,
+				Secure:   *secure,
+			}
+
+			flooder.Flood()
+		}
+		for i := 0; i < *threads; i++ {
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+
+				flooder := internals.HttpFlooder{
+					Host:     *host,
+					Port:     *port,
+					Duration: *duration,
+					Interval: *interval,
+					Secure:   *secure,
+				}
+
+				flooder.Flood()
+			}()
+		}
+	}
+
+	wg.Wait()
 }
